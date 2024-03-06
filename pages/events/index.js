39e52951caf873d704c71eb52f82b7eae29a3ee9
Hazhir2002 <HazhirYousefi2002@gmail.com>
2024-03-06 15:38:30 +0330

    Done some changes

---
+++ pages/events/index.js
@@ -0,0 +1,24 @@
+import EventList from "@/components/events/event-list";
+import { getAllEvents } from "@/dummy-data";
+import EventsSearch from "@/components/events/event-search";
+import { useRouter } from "next/router";
+
+function AllEventsPage() {
+  const router = useRouter();
+  const events = getAllEvents();
+
+  function findEventsHandler(year, month) {
+    const fullPath = `/events/${year}/${month}`;
+
+    router.push(fullPath);
+  }
+
+  return (
+    <div>
+      <EventsSearch onSearch={findEventsHandler} />
+      <EventList items={events} />
+    </div>
+  );
+}
+
+export default AllEventsPage;
